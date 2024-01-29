ca04b4326fea429da8c3e128b8925b8cf968a312
Felipe Dórea <felipeldorea@gmail.com>
2024-01-29 19:09:58 -0300

    updates

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,4 +2,6 @@ vim.cmd("set noexpandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu rnu")
+vim.cmd("set cursorline")
+vim.cmd("set colorcolumn=80")
 vim.g.mapleader = " "
