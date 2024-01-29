106faac383f6f79668da8ea6d71719aaf1ce55c1
flpdorea <felipeldorea@gmail.com>
2024-01-29 15:54:25 -0300

    big update

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd"
+				}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+			vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+			vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename)
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+		end
+	}
+}
---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "gruvbox",
+				-- component_separators = { left = '', right = '' },
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' }
+			},
+			sections = {
+				lualine_a = {"mode"},
+				lualine_b = {"branch", "diff", "diagnostics"},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {"location" , "progress"}
+			}
+		})
+	end
+}
